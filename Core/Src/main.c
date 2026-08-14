abb79ce6ac01da0758b73c0a6d8ee75b80164ed4
Andrew Han <kihan@snut.ac.kr>
2026-08-14 11:43:06 +0900

    HAL_UARTEx_ReceiveToIdle_DMA

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "dma.h"
+#include "stm32f4xx_hal_uart.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -26,6 +27,7 @@
 /* USER CODE BEGIN Includes */
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #ifdef __GNUC__
   #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 #else
@@ -38,7 +40,7 @@ PUTCHAR_PROTOTYPE
   HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
   return ch;
 }
-#define RX_BUF_SIZE 2
+#define RX_BUF_SIZE 128
 uint8_t rx_data;
 uint8_t rx_buf[RX_BUF_SIZE];
 /* USER CODE END Includes */
@@ -88,8 +90,22 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
     else
       HAL_UART_Transmit(&huart2, rx_buf, RX_BUF_SIZE, 100);
 
-    HAL_UART_Receive_DMA(&huart2, rx_buf, RX_BUF_SIZE);
+    HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf, RX_BUF_SIZE);
   }
+}
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
+{
+  if(huart->Instance==USART2){
+    if(rx_data=='a')
+      printf("Hello STM32 Cortex-M4 USART Polling!\r\n");
+    else
+      HAL_UART_Transmit(&huart2, rx_buf, Size, 100);
+
+    HAL_UART_DMAStop(&huart2);
+    memset(rx_buf,0,Size);
+  }
+  HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf, RX_BUF_SIZE);
+
 }
 
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart){
@@ -133,7 +149,7 @@ int main(void)
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
  //HAL_UART_Receive_IT(&huart2, &rx_data, 1);
-  HAL_UART_Receive_DMA(&huart2, rx_buf, RX_BUF_SIZE);
+  HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf, RX_BUF_SIZE);
 
   /* USER CODE END 2 */
 
