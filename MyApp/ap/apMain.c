6c623a85fa98707e2ac4d8920384ffeb4f24a3bb
Andrew Han <kihan@snut.ac.kr>
2026-08-18 11:41:58 +0900

    컴포넌트 구조로 변경

---
+++ MyApp/ap/apMain.c
@@ -0,0 +1,16 @@
+#include "apMain.h"
+#include "myUart.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+void apInit(void){
+    uartInit();
+}
+
+void apMain(void){
+    while(1){
+
+    }
+}
---
+++ MyApp/ap/apMain.h
@@ -0,0 +1,6 @@
+#include "main.h"
+
+
+void apInit(void);
+
+void apMain(void);
