b87c85918e641293507e4d3ca1051f6beca7b3fb
Andrew Han <kihan@snut.ac.kr>
2026-08-19 16:35:35 +0900

    dht11 디버깅 마무리

--- MyApp/ap/apMain.c
+++ MyApp/ap/apMain.c
@@ -1,6 +1,7 @@
 #include "apMain.h"
 #include "myAdc.h"
 #include "myUart.h"
+#include "myDht11.h"
 #include "stm32f411xe.h"
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_adc.h"
@@ -15,15 +16,20 @@ extern ADC_HandleTypeDef hadc1;
 void apInit(void) { 
   uartInit();
   adcInit();
+  dht11Init();
   
 }
 
 float internal_temp=0;
-
+dht11Data_t dht_data={0};
+bool dht_status=false;
 void apMain(void) {
 
   while (1) {
     adcUpdate();
+
+    dht_status=dht11Read(&dht_data);
+
     internal_temp=adcGetTemp();
     HAL_Delay(10);
   }
