c415b652816f0bd0c2cd6272bc1ae02d9ab76825
Andrew Han <kihan@snut.ac.kr>
2026-08-24 16:52:58 +0900

    pwm 중간-코딩 완료안됨

--- MyApp/hw/driver/myTimer.c
+++ MyApp/hw/driver/myTimer.c
@@ -1,7 +1,27 @@
 #include "myTimer.h"
 #include "stm32f4xx_hal_rtc.h"
 #include "rtc.h"
+#include "stm32f4xx_hal_tim.h"
+#include "tim.h"
 #include <stdint.h>
+
+void timerInit(void){
+    timerPwmStart();
+    timerSetDuty(50);
+}
+
+void timerPwmStart(void){
+    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+}
+
+void timerPwmStop(void){
+    HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_1);
+}
+
+void timerSetDuty(uint8_t duty_percent){
+    uint32_t period = __HAL_TIM_GET_AUTORELOAD(&htim2)+1;
+}
+
 
 void RTC_Set_Next_10s_Alarm(void){
     RTC_TimeTypeDef sTime={0};
